4bb3945cdf642181fd0ef82911454fc9f16db5f8
SloneZZ <39183335+SloneZZ@users.noreply.github.com>
2021-01-20 18:57:10 +0100

    Server v3

--- routes/countries.js
+++ routes/countries.js
@@ -6,7 +6,7 @@ const databaseUser = 'Raphael:coroverview123'
 const url = `mongodb+srv://${databaseUser}@corona-overview-app.cgz9f.mongodb.net/${databaseName}?retryWrites=true&w=majority`
 const mongoClient = require('mongodb').MongoClient
 
-const countryModel = require('../models/country')
+const country_codes_package = require('country-calling-code');
 
 router.get('/countries', (req, res, next) => {
     mongoClient.connect(url, (error, client) => {
@@ -27,6 +27,33 @@ router.get('/countryViaCode', (req, res, next) => {
             res.send(result)
         })
     })
+})
+
+router.get('/countryViaDialcode', (req, res, next) => {
+    const dialCode = req.query.dialcode
+    const dialCodeTrimmed = dialCode.substring(1, dialCode.length)
+
+    const codes = country_codes_package.codes
+
+    let codeIsCorrect = false
+
+    for (var i = 0; i < codes.length; i++) {
+        for (var j = 0; j < codes[i].countryCodes.length; j++) {
+            if (codes[i].countryCodes[j] === dialCodeTrimmed) {
+                codeIsCorrect = true
+            }
+        }
+        if (codeIsCorrect) {
+            mongoClient.connect(url, (error, client) => {
+                const db = client.db('coroverview_db')
+                db.collection('country_full').findOne({country_code: codes[i].isoCode3}, (err, result) => {
+                    if (err) res.send(err)
+                    res.send(result)
+                })
+            })
+            break
+        }
+    }
 })
 
 module.exports = router;
